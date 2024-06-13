f2a2aa72ea6af129d629977e33943e4b3cc23fbd
Muhammadyusuf <mmuhammadyusuf302@gmail.com>
2024-06-13 19:54:49 +0500

    Admin panel qayta yukladim

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Dashboarda } from './Components/Dashboard/Dashboard'; 
+import { Brends } from './Components/Categories/Brends/Brends';
+import { Cars } from './Components/Categories/Cars/Cars';
+import { Cities } from './Components/Categories/Cities/Cities';
+import { Models } from './Components/Categories/Models/Models';
+import {Settings} from "./Components/Categories/Settings/Settings"
+import { Login } from './Components/Login/Login'; 
+
+function App() {
+	
+  return (
+    <div>
+      <Router>
+      <Routes>
+	   <Route path="/" element={<Login />} />
+        <Route path="/panel" element={<Dashboarda />}>
+          <Route path="/panel/settings" element={<Settings />} />
+          <Route path="/panel/cities" element={<Cities />} />
+          <Route path="/panel/cars" element={<Cars />} />
+          <Route path="/panel/brends" element={<Brends />} />
+		  <Route path="/panel/models" element={<Models />} />
+        </Route>
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import "../Login/Login.css";
+
+export const Login = () => {
+  const [phone, setPhone] = useState("");
+  const [parol, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://autoapi.dezinfeksiyatashkent.uz/api/auth/signin",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            phone_number: phone,
+            password: parol,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+
+      const data = await response.json();
+      localStorage.setItem('accessToken',23)
+	  console.log(localStorage.getItem('accessToken'))
+
+      navigate("/panel");
+    } catch (error) {
+      console.error("Error logging in:", error);
+      setError("Login failed. Please check your credentials and try again.");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        {error && <p className="error">{error}</p>}
+        <div className="form-group">
+          <label htmlFor="phone">Login</label>
+          <input
+            type="text"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e?.target?.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="parol">Password</label>
+          <input
+            type="parol"
+            id="parol"
+            value={parol}
+            onChange={(e) => setPassword(e?.target?.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
